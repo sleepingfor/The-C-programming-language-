a34b13abf669d9e0eb4ad8a9391e88173bcecb96
yanmengyang <yanmengyang8@163.com>
2023-05-11 15:20:35 +0800

    excise1_1.c is printf("Hello,world!")

---
+++ Chapter1/excise1_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* when fahr = 0, 20 , ... ,300,
+printing the table of comparing 
+fahr with celsius */
+
+main()
+{
+  int fahr , celsius;
+  int lower , upper , step;
+
+  lower = 0;    /* The tem
+
+
+}
+
