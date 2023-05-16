39516c3b4ddd6a4e69b7f6a04de4a96bbddb3215
yanmengyang <yanmengyang8@163.com>
2023-05-16 14:47:35 +0800

    exercise 1-18

--- Chapter1/exercise1-18.c
+++ Chapter1/exercise1-18.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 
-#define MAXLINE 1000   /* maximum imput line length */
-#define LENGTH  80     /* print length of lines     */
+#define MAXLINE  1000   /* maximum input line length                       */
+#define NEW_TERM  2     /* one newline character and one string terminator */
+/* remove trailing blanks and tabs from each line of input , and to delete entirely blank lines*/
 
-int getlines(char line[] , int maxline);
+int getlines(char line[] , int maxline );
 
-/*print the longest input line*/
-int main()
+/**/
+int main() 
 {
-  int len;              /* current line length       */
-  char line[MAXLINE];   /* current input line        */
-  char longest[MAXLINE];/* longest line saved here   */
+  int len;                /* current line length subtract one newline character and one string terminator */
+  int real_len;           /* line length remove trailing blanks and tabs , '\0' , '\n'*/
+  char line[MAXLINE];     /* maximum length seen so for                   */
+  char del_line[MAXLINE]; /*store the line after deleting the tabs,blanks */
 
-  while((len = getlines(line , MAXLINE)) > 0) {
-    if (len > LENGTH) /* there was a line*/
-      printf("%s" , line);
+  while ((len = getlines(line , MAXLINE)) > 0) {
+    len = len - NEW_TERM;
+    while(line[len] == ' ' || line[len] == '\t') {
+        real_len = len ;
+        --len;
     }
-  return 0;
+
+    if (real_len != 0) {
+      int i;
+      for( i = 0; i < real_len ; ++i) 
+        printf("%c" , line[i]);
+
+      printf("\n");
+    }
+  }
 }
 
-/* getline : read a line into s , return length */
-int getlines(char s[], int lim)
+/* getline: read a line into s, return length */
+int getlines(char s[] , int lim)
 {
   int c, i;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n' ; ++i)
+  for(i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n' ; ++i)
     s[i] = c;
   if (c == '\n') {
     s[i] = c;
@@ -33,5 +45,3 @@ int getlines(char s[], int lim)
   s[i] = '\0';
   return i;
 }
-
-
