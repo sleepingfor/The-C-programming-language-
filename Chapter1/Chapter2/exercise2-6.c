8d58d7b7260134e6c8db0ce98c9c4d1292fe59b8
yanmengyang <yanmengyang@163.com>
2023-08-01 14:28:19 +0800

    test the exercise2-6.c

--- Chapter1/Chapter2/exercise2-6.c
+++ Chapter1/Chapter2/exercise2-6.c
@@ -1,7 +1,19 @@
+#include<stdio.h>
 /* a function setbits: return x with the n bits that begin
    at position p set to the rightmost n bits of y,leaving 
    the other bits unchanged                               */
-void setbits(unsigned x,int p,int n,unsigned y) {
+int setbits(unsigned x,int p,int n,unsigned y) {
   y = (y << n+1) | (x >> (p+1-n)) & (~0 << n);
   return ((x >> p+1) << (p+1-n)) | (x & (~0 << p-n));
+}
+
+int main() {
+  unsigned x = 0b10010;
+  int p = 3;
+  int n = 1;
+  unsigned y = 0b10;
+  printf("x is %d\n",setbits(x,p,n,y));
+  //printf("y is %u\n",y);
+
+
 }
